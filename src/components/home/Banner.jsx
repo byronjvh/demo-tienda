a9a58ab21352cc5f61ea54861b522c7b9f48d6d5
JordyV23 <jordyvargas0123@gmail.com>
2025-10-25 21:53:45 -0600

    UPD: Banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -3,7 +3,7 @@ import { BadgeCheck } from 'lucide-react';
 export const Banner = () => {
   return (
     <>
-      <header className="flex justify-evenly text-nowrap py-2 items-center bg-crtech-grey text-white">
+      <header className="flex justify-evenly text-nowrap py-6 items-center bg-crtech-grey text-white">
         <p className="text-lg flex items-center gap-1">
           <span className="text-xl">
             <BadgeCheck color="#ff5800" />
@@ -15,11 +15,11 @@ export const Banner = () => {
           <p> Llámanos: La V 9:00 AM a 6:00 PM (+506) 22341002</p>
         </div>
         <div className="flex gap-2 text-sm">
-          <button className="flex gap-2 cursor-pointer">Tiendas</button>
+          <a className="flex gap-2 cursor-pointer">Tiendas</a>
           <span className="flex gap-2 cursor-pointer">|</span>
-          <button className="flex gap-2 cursor-pointer">
+          <a className="flex gap-2 cursor-pointer hover:text-orange-600" href='https://www.crtechgaming.com'>
             www.crtechgaming.com
-          </button>
+          </a>
         </div>
       </header>
     </>
