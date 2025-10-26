8d6bd33bcb762bf754e62986d368792ce3a2cd6f
Byron Vargas <byronjvh02@gmail.com>
2025-10-26 14:52:01 -0600

    add: Header component and rendering it global

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Product from './pages/Product.jsx'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/:sku" element={<Product />} />
