0dbf99fddff5995f98056f31225dba090c337f11
Byron Vargas <byronjvh02@gmail.com>
2025-10-26 19:47:22 -0600

    UPD: Se agregó link al logo para volver a home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Search } from 'lucide-react';
 import { Banner } from './home/banner';
 import { Cart } from './Icons';
+import { Link } from 'react-router';
 
 export default function Header() {
     return (
         <>
             <Banner />
             <div className='grid grid-cols-3 items-center gap-8 py-6'>
-                <img className='w-56 place-self-center' src="https://cdn-ilaabjh.nitrocdn.com/OzMRamyAKVKkRfGWMkLhcUdtvVYEmpJD/assets/images/optimized/rev-ecca6a1/crtechstore.com/wp-content/uploads/2020/06/Logo-cr-tech-06.png" alt="logo" />
+                <Link to="/">
+                    <img className='w-56 place-self-center' src="https://cdn-ilaabjh.nitrocdn.com/OzMRamyAKVKkRfGWMkLhcUdtvVYEmpJD/assets/images/optimized/rev-ecca6a1/crtechstore.com/wp-content/uploads/2020/06/Logo-cr-tech-06.png" alt="logo" />
+                </Link>
                 <div className='place-self-center border border-gray-400 rounded-xl h-12 flex gap-1.5 w-full max-w-96 items-center p-1.5 '>
                     <input className='w-full h-full p-1' type="text" placeholder='Encuentra lo que buscas' />
                     <button className='h-full aspect-square rounded-lg flex justify-center items-center bg-orange-500 text-white cursor-pointer'><Search width="22" /></button>
